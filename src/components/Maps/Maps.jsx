f9a5a805fe7787313c201f36418dd6dc0b4aa9e3
Zhangirrr <zangiruakasov@gmail.com>
2023-06-23 10:52:37 +0600

    Карта готова, нужно только подкорректировать метки.

--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { useEffect, useRef, useState, useMemo } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 import s from "./Maps.module.scss";
-import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
 const endpoint = process.env.REACT_APP_ENDPOINT || "";
 
-const Maps = () => (
-  <YMaps>
-    <div>
-      <Map
-        className={s.map}
-        defaultState={{
-          center: [51.143974, 71.435806],
-          zoom: 5,
-          controls: ["zoomControl", "fullscreenControl"],
-        }}
-        modules={["control.ZoomControl", "control.FullscreenControl"]}
-      >
-        {/* <Placemark defaultGeometry={[`${endpoint.}`]} /> */}
-      </Map>
-    </div>
-  </YMaps>
-);
+const Map = () => {
+  const mapRef = useRef(null);
+  const [coords, setCoords] = useState([]);
 
-export default Maps;
+  useEffect(() => {
+    fetch(`${endpoint}/markers`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCoords(data);
+        // console.log(data);
+      });
+
+    mapRef.current = L.map("map", {
+      center: [52.285577, 76.940722],
+      zoom: 5,
+    });
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "Карта принадлежит Асель и Жангир &copy;",
+    }).addTo(mapRef.current);
+
+    return () => {
+      mapRef.current.remove();
+    };
+  }, []);
+
+  useMemo(() => {
+    if (coords.length > 0) {
+      coords.forEach((item) => {
+
+        const customIcon = L.icon({
+          iconUrl: item.icon,
+          iconSize: [20, 20], 
+          iconAnchor: [10, 10], 
+        });
+
+        const marker = L.marker([item.lat, item.lon], {
+          icon: customIcon,
+          alt: item.city,
+        }).addTo(mapRef.current);
+
+        marker.bindTooltip(item.city, {
+          permanent: true, 
+          direction: "top", 
+          className: s.cityLabel, 
+        });
+      });
+    }
+  }, [coords]);
+
+  return <div id="map" className={s.map}></div>;
+};
+
+export default Map;
